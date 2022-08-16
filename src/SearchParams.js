9993a2685cf441305541f51ac2bd3400e5455bc8
= <=>
2022-08-16 11:27:37 +0530

    Version : 1.3.0
    NO I use useState hook onto the component it let me fetch and update state's value without using "this" it returns array of variable the first one is used to show the value and seconde one is to set the value;

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,20 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 const SearchParams = () => {
-  let location = "Seattle, WA";
+  //   let location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA");
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           location
-          <input id="location" value={location} placeholder="Location...." />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location...."
+            onChange={(e) => setLocation(e.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
